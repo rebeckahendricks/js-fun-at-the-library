9b60feecd9034340abc1d23c3718082f2620d8de
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2024-04-02 14:11:50 -0600

    Finish shelf part of project

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,31 @@
+function shelfBook(book, shelf) {
+  if (shelf.length >= 3) {
+    return;
+  }
+
+  shelf.unshift(book);
+};
+
+function unshelfBook(titleToRemove, shelf) {
+  const index = shelf.findIndex(book => book.title === titleToRemove);
+  shelf.splice(index, 1);
+};
+
+function listTitles(shelf) {
+  return shelf.map(book => book.title).join(', ');
+};
+
+function searchShelf(shelf, bookTitle) {
+  if (shelf.find(book => book.title === bookTitle)) {
+    return true;
+  };
+
+  return false;
+};
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
